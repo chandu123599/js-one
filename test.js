41c04118c745518ecc4e04425ada37d0e0e4df8e
chandu123599 <p.chandusasidhar@gmail.com>
2020-12-08 19:26:36 +0530

    js

---
+++ test.js
@@ -0,0 +1,46 @@
+//operations is javascript
+
+//arithmetic is operation
+
+let numfive = 20;
+let numTwo =20;
+//console.log(numfive + numTwo)
+//console.log(numOne + numTwo)
+//console.log(numOne - numTwo)
+//console.log(numOne * numTwo)
+//console.log(numOne / numTwo)
+
+//increment and decrement operators
+let x = 10
+//console.log(x++)= 10
+//console.log(++x)=11
+//console.log(--x)= 9
+//console.log(x--)= 10
+//console.log(x++ + 10) = 10+10=20
+//console.log(--x + 10)= 9+10=19
+//console.log(x-- + 10)=10+10=20
+//console.log(x)= 10
+//console.log(x + 10)=10+10=20
+//console.log(2+x)= 2+10=12
+let a =50
+let b=100
+//console.log(--a+b--)=149
+//console.log(a++)=50
+//console.log(++a)=51
+//console.log(--a)=49
+//console.log(a--)=50
+
+//arithmetic is operation
+
+//assg 
+let d =3;
+//console.log(d =d+3);=3+3=6
+//console.log(d =d-3);=6-3=3
+//console.log(d =d/3)//;=3*3=9
+//console.log(d =d*3)//;=9/3=3
+//assg
+
+console.log(10>20)
+console.log(30<40)
+
+
